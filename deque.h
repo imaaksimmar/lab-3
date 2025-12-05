b9dbb69d90215d984f3ae5712729ebb7421afc03
яна <imaksimmar@gmail.com>
2025-12-05 21:50:38 +0300

    первая реализация дека

---
+++ deque.h
@@ -0,0 +1,28 @@
+struct element {
+    int data;
+    struct element *prev;
+    struct element *next;
+};
+typedef struct element elem;
+
+
+typedef struct my_deque {
+    elem *start;
+    elem *end;
+    int size;
+} my_deque;
+
+my_deque* create_deque(void);
+
+void add_front(my_deque *dq, int data);
+void add_back(my_deque *dq, int data);
+void add_at_index(my_deque *dq, int index, int data);
+
+
+void pop_front(my_deque *dq);
+void pop_back(my_deque *dq);
+void pop_at_index(my_deque *dq, int index);
+
+void print_deque(my_deque *dq);
+void free_deque(my_deque *dq);
+
